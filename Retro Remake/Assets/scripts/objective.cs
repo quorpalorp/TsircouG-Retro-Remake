9e62a6d3df1d57baa5a258ca5af07d1f04a5d43d
gtsircou <1722710@mukilteo.wednet.edu>
2024-09-27 09:28:39 -0700

    wake up in the morning feeling like p diddy

--- Retro Remake/Assets/scripts/objective.cs
+++ Retro Remake/Assets/scripts/objective.cs
@@ -15,6 +15,9 @@ public class objective : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       // if (objectiveCount == 3)
+        {
+
+        }
     }
 }
